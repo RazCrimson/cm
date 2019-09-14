bb23f62dc182169f18f0c0dd538af304f8dabaaf
RazCrimson3 <bharathvigneshjo@gmail.com>
2019-09-14 11:21:49 -0500

    Setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
+#bash script to add to path, clear clipboard and compile the file as cm
 PATH=$PATH:$PWD/cm
-replace=$PWD/clipboard
-rm replace; touch replace
-file=abc.txt
-sed -s "rsclipboard/$replace/g" $file > $file.new
-mv $file.new $file
+path=$PWD/clipboard
+rm path; touch path
+file=cm1.c
+sed -i 's|clipboard_path_added_by_bash_script|'$path'|g' $file > $file.new
+#rm -f $file
+#mv $file.new $file
+gcc -O $file -o cm
