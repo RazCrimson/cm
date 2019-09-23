adde94ee4b3830b8e0963fd0c5b5e30be0cba79a
root <root@localhost.localdomain>
2019-09-23 20:58:03 +0000

    Final Ver 0.6 (paste still needs work)

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #bash script to add to path, clear clipboard and compile the file as cm
 PATH=$PATH:$PWD/cm
 path=$PWD/.clipboard
-rm path; touch path
+rm $path; touch $path
 file=cm.c
 sed -i 's|clipboard_path_added_by_setup.sh|'$path'|g' $file > output.c 
 gcc -O output.c -o cm
